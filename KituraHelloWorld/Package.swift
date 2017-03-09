9353718490c1901d09b696be892e820754d10f0a
Carl Brown <carl.brown1@ibm.com>
2017-03-08 22:26:05 -0600

    Update kitura for Swift 3.0.2

--- KituraHelloWorld/Package.swift
+++ KituraHelloWorld/Package.swift
@@ -25,8 +25,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 10),
-        //.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 13),
+ .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/kylef/Stencil", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/kylef/Spectre", majorVersion: 0, minor: 7),
     ],
     exclude: ["Makefile", "Kitura-Build"])
